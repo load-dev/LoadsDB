0cf51d6011e7025945373c7cf1a981e44fceb153
LoadsX <122114788+LoadsX@users.noreply.github.com>
2023-01-07 16:41:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='LoadsDB',
+    version='1.0.0',
+    packages=[''],
+    include_package_data=True,
+    install_requires=[
+    ],
+    author='Loads',
+    author_email='contact.loadsx@gmail.com',
+    description='Working database system for python!',
+    keywords='LoadsDB database',
+    url='https://github.com/your-username/mypackage',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
